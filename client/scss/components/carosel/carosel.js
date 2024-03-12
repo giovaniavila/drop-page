f4faa6b6caf8143f818cecabf4a94cd221d5dc1d
giovaniavila <giovanicarvalhoavila@hotmail.com>
2024-03-12 18:39:29 -0300

    :fire: remove diretorios inutilizados

--- client/scss/components/carosel/carosel.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll("[data-carosel-arrowLeft]")
-
-buttons.forEach(buttons => {
-    buttons.addEventListener("click", () => {
-        const offset = buttons.dataset.carouselButton === "next" ? 1 : -1
-        const slides = buttons.closest("[data-carousel]").querySelector('[data-slides]')
-    })
-})
--- client/scss/components/slide/java.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elem = document.querySelector('.main-carousel');
-  var flkty = new Flickity(elem, {
-    // Opções
-    cellAlign: 'left',
-    contain: true
-  });
-});
